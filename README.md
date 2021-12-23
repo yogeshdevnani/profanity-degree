70407caaf7cd8f3a9913b539e917886109709816
yogeshdevnani <yogeshdevnani@hotmail.com>
2021-12-23 14:44:10 +0530

    Updated README.md and improved flow.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 A program to indicate the degree of profanity in text.
+
+This script gets the data(twitter's data) from data folder. The mainData.txt has all the data that will be parsed. You can put other text files in the folder and update those by running the program in a Python editor.
+Dictionary contains 'wordsToLook.txt', that contains the word that will be flagged. You may add other text files and append/ replace the existing words by running the program.
+
+The degree of profanity is calculated by : {[(noOfOffensiveWords)/ (totalNumberOfWords)] + [(noOfOffensiveWords)/ (totalNumberOfSentences)]} / 2 - It gives a better degree of profanity, as it includes the number of sentences
+Number of sentences are calculated using nlkt (dependency included).
--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 #It just checks for words from a hard-coded dictionary.
 
 import os
+import nltk
+
 from nltk import tokenize
 
 
-def main():
+def processWords():
     # colors are used to describe other things as well
     # hence, if a {skin color} + attribute is what you are looking for - add brown guy
     # so next time any sentence that just uses the color black won't be marked as offensive
@@ -20,21 +22,27 @@ def main():
         offenseCount = 0
         for offensiveWord in dictList:
             if offensiveWord in sentence:
-                numberSentence =  len(tokenize.sent_tokenize(sentence))
+                try:
+                    numberSentence =  len(tokenize.sent_tokenize(sentence))
+                except:
+                    print ("Seems, this is the first time for this program on this machine. \nDownloading a dependency.\t")
+                    nltk.download('punkt')
+                    numberSentence = len(tokenize.sent_tokenize(sentence))
                 offenseCount += 1
-                print (f'{"found", offensiveWord, sentence}')
-                print (f'{"Profanity Degree ", degreeCalc(offenseCount, len(sentence.split()),numberSentence)}')
-
+                print (sentence,end=" ")
+                print ("Profanity Degree: ", degreeCalc(offenseCount, len(sentence.split()),numberSentence))
+                print ("")
 
 
 def addWord():
     wordToAdd = input("Type in the word that I missed: - Type 'exit' to exit \t")
+    print (wordToAdd)
     if wordToAdd == 'exit':
         return
     elif (wordToAdd == ""):
         addWord()
-    dictAddWords.write(wordToAdd+"\n")
-    addWord()
+    dictAddWords.write(wordToAdd + "\n")
+
 
 
 def modifyWords():
@@ -54,12 +62,21 @@ def modifyWords():
         if tempInput.lower() == 'y':
             os.remove(tempDir)
     elif temp == '2':
-        dictWriteWords = open("dictionary/wordsLevel1.txt", 'w')
+        dictWriteWords = open("dictionary/wordsToLook.txt", 'w')
         dictWriteWords.write(newWordList.read())
+        dictWriteWords.write("\n")
         newWordList.close()
         os.remove(tempDir)
         dictWriteWords.close()
 
+def changeData():
+    fileNames = os.listdir("data")
+    for i in range(len(fileNames)):
+        print (i+1,"\t",fileNames[i])
+    temp = int(input("Enter new file number to replace with existing data:\t"))
+    tempDir = "data/" + fileNames[temp-1]
+    closeProgram()
+    os.replace(tempDir, "data/mainData.txt")
 
 
 def degreeCalc(offensiveCount, totalCount,numberSentence):
@@ -76,31 +93,33 @@ def closeProgram():
 
 
 def menu():
-    print ('1. View Results')
-    print ('2. Add a new word')
-    print ('3. Modify Word List')
-    print ('4. Change Data Set')
-    print ('5. View ReadMe')
-    print ('0. Search Using GOD Mode')
-    print ("Enter 'e' to exit")
-    temp = input("Enter your choice:\t")
-    if temp == '1':
-        main()
-    elif temp == '2':
-        addWord()
-    elif temp == '3':
-        #modify WordList
-        modifyWords()
-    elif temp == '4':
-        print('')
-    elif temp == '5':
-        print()
-    elif temp == '0':
-        print()
-    elif temp.lower() == 'e':
-        closeProgram()
+    while True:
+        print ('1. View Results')
+        print ('2. Add a new word')
+        print ('3. Modify Word List')
+        print ('4. Change Data Set')
+        print ('5. View ReadMe')
+        print ("Enter 'exit' to exit")
+        temp = input("Enter your choice:\t")
+        if temp == '1':
+            processWords()
+        elif temp == '2':
+            addWord()
+        elif temp == '3':
+            modifyWords()
+        elif temp == '4':
+            changeData()
+            print ("Updated Successfully. Please open the program again to use.")
+            break
+        elif temp == '5':
+            os.startfile("README.md")
+        elif temp.lower() == 'exit':
+            closeProgram()
+            print ("Closing program.")
+            break
 
-dataFile = open("data/content.txt",'r')
-dictWords = open("dictionary/wordsLevel1.txt",'r')
-dictAddWords = open('dictionary/wordsLevel1.txt', 'a')
+
+dataFile = open("data/mainData.txt",'r')
+dictWords = open("dictionary/wordsToLook.txt",'r')
+dictAddWords = open('dictionary/wordsToLook.txt', 'a')
 menu()
